5c574c2ceaf5d9e440bc60f79187d282b78a9448
MacPro <macpro@creative-ones.com>
2022-11-19 23:49:03 +0200

    Initial Commit

---
+++ inty/intyApp.swift
@@ -0,0 +1,17 @@
+//
+//  intyApp.swift
+//  inty
+//
+//  Created by Creative Ones on 11/19/22.
+//
+
+import SwiftUI
+
+@main
+struct intyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
